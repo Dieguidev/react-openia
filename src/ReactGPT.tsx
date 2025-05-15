63284917e0c8df53f60519810c48625681500a10
Dieguidev <diegogaraycullas@gmail.com>
2025-05-14 21:16:06 -0500

    Implementar la estructura de enrutamiento y agregar nuevas páginas al proyecto

--- src/ReactGPT.tsx
+++ src/ReactGPT.tsx
@@ -1,7 +1,9 @@
+import { RouterProvider } from "react-router-dom"
+import { router } from './presentation/router/router';
 
 
 export const ReactGPT = () => {
   return (
-    <h1 className="">ReactGPT</h1>
+    <RouterProvider router={router} />
   )
 }
---
+++ src/presentation/pages/text-to-audio/TextToAudioPage.tsx
@@ -0,0 +1,7 @@
+
+
+export const TextToAudioPage = () => {
+  return (
+    <div>TextAudioPage</div>
+  )
+}
